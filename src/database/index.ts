ba8e630efa9396a0b4cd7cdcfc17974c3d711a1a
YuriRibeiroRosa <yurixribeirorosa@gmail.com>
2024-02-13 14:39:00 -0300

    Configurando o Sequelize

---
+++ src/database/index.ts
@@ -0,0 +1,14 @@
+import { Sequelize } from "sequelize";
+
+export const sequelize = new Sequelize ({
+  dialect: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  database: 'onebitflix_development',
+  username: 'onebitflix',
+  password: 'onebitflix',
+  define: {
+    underscored: true
+  }
+})
+
