131ced8fe5ff91c03313a1748c00b426c652fa13
YuriRibeiroRosa <yurixribeirorosa@gmail.com>
2024-02-21 00:25:54 -0300

    Refatoração configs AdminJs

--- src/adminjs/index.ts
+++ src/adminjs/index.ts
@@ -3,9 +3,10 @@ import AdminJSExpress from "@adminjs/express"
 import AdminJSSequelize from "@adminjs/sequelize"
 import { sequelize } from "../database";
 import { adminJsResources } from "./resources";
-import { Category, Course, Episode, User } from "../models";
-import bcrypt from "bcrypt"
 import { locale } from "./locale";
+import { dashboardOptions } from "./dashboard";
+import { brandingOptions } from "./branding";
+import { authenticateOptions } from "./authentication";
 
 AdminJS.registerAdapter(AdminJSSequelize)
 
@@ -13,60 +14,12 @@ export const adminJs = new AdminJS({
   databases: [sequelize],
   rootPath: '/admin',
 	resources: adminJsResources,
-  branding: {
-    companyName: 'OneBitFlix',
-    logo: '/logoOnebitflix.svg',
-    theme: {
-      colors: {
-        primary100: '#ff0043',
-	      primary80: '#ff1a57',
-	      primary60: '#ff3369',
-	      primary40: '#ff4d7c',
-		    primary20: '#ff668f',
-	      grey100: '#151515',
-	      grey80: '#333333',
-	      grey60: '#4d4d4d',
-	      grey40: '#666666',
-	      grey20: '#dddddd',
-	      filterBg: '#333333',
-	      accent: '#151515',
-	      hoverBg: '#151515',
-      }
-    }
-  },
+  branding: brandingOptions,
 	locale: locale,
-	dashboard: {
-		component: AdminJS.bundle("./components/Dashboard"),
-		handler: async (req, res, context) => {
-			const courses = await Course.count()
-			const episodes = await Episode.count()
-			const categories = await Category.count()
-			const standardUsers = await User.count({ where: { role: 'user' } })
-			
-			res.json({
-				'Cursos': courses,
-				'Episódios': episodes,
-				'Categorias': categories,
-				'Usuários': standardUsers
-			})
-		}
-	}
+	dashboard: dashboardOptions
 })
 
-export const adminjsRouter = AdminJSExpress.buildAuthenticatedRouter(adminJs, {
-	authenticate: async (email, password) => {
-		const user = await User.findOne({ where: { email} })
-		if (user && user.role === 'admin') {
-			const matched = await bcrypt.compare(password, user.password)
-
-			if(matched) {
-				return user
-			}
-		}
-		return false
-	},
-	cookiePassword: 'senha-do-cookie'
-}, null, {
+export const adminjsRouter = AdminJSExpress.buildAuthenticatedRouter(adminJs, authenticateOptions, null, {
 	resave: false,
 	saveUninitialized: false
 })
