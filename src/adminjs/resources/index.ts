ee4a52e9d56802b7b826819c9a644d1f36e6081d
YuriRibeiroRosa <yurixribeirorosa@gmail.com>
2024-02-20 21:43:07 -0300

    Novas tabelas, e seeders

--- src/adminjs/resources/index.ts
+++ src/adminjs/resources/index.ts
@@ -1,7 +1,8 @@
 import { ResourceWithOptions } from "adminjs";
-import { Category, Course } from "../../models";
+import { Category, Course, Episode } from "../../models";
 import { categoryResourceOptionns } from "./category";
 import { courseResourceOptions } from "./course";
+import { episodeResourceOptions } from "./episodes";
 
 export const adminJsResources: ResourceWithOptions[] = [
   {  resource: Category,
@@ -10,5 +11,9 @@ export const adminJsResources: ResourceWithOptions[] = [
   {
     resource: Course,
     options: courseResourceOptions
+  },
+  {
+    resource: Episode,
+    options: episodeResourceOptions
   }
 ]
