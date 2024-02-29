8c79a2930f4d5482171e1a9cf1303ea54d95825a
YuriRibeiroRosa <yurixribeirorosa@gmail.com>
2024-02-29 11:48:05 -0300

    Middleware de autorização

--- src/services/jwtService.ts
+++ src/services/jwtService.ts
@@ -7,5 +7,9 @@ export const jwtService = {
     return jwt.sign(payload, secret, {
       expiresIn: expiration
     })
+  },
+
+  veriftToken: (token: string, callbackfn: jwt.VerifyCallback) => {
+    jwt.verify(token, secret, callbackfn)
   }
 }
