1e6b62d204bead3cec37d2fbc9024c0ae8f4f441
YuriRibeiroRosa <yurixribeirorosa@gmail.com>
2024-02-29 17:40:44 -0300

    Endpoint para like nos cursos

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,13 +1,15 @@
 import { User } from './User';
 import { Category } from "./Category";  
 import { Course }  from "./Course";
-import { Favorite } from './Favoreite';
+import { Favorite } from './Favorite';
 import { Episode } from "./Episode";
+import { Like } from './Like';
 
 Category.hasMany(Course, { as: 'courses'})
 
 Course.belongsTo(Category)
 Course.belongsToMany(User, { through: Favorite})
+Course.belongsToMany(User, { through: Like})
 Course.hasMany(Episode, { as: 'episodes'})
 Course.hasMany(Favorite, { as: 'FavoritesUsers', foreignKey: 'course_id'})
 
@@ -17,6 +19,7 @@ Favorite.belongsTo(Course)
 Favorite.belongsTo(User)
 
 User.belongsToMany(Course, { through: Favorite})
+User.belongsToMany(Course, { through: Like})
 User.hasMany(Favorite, { as: 'FavoritesCourse', foreignKey: 'user_id'})
 
 export {
@@ -24,5 +27,6 @@ export {
   Course,
   Episode,
   Favorite,
+  Like,
   User
 }
